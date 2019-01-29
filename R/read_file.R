c02b9b32720c789e9ced4f5e0171588daf063ae6
Vitalina Komashko <vitalina@gmail.com>
2019-01-29 11:07:59 -0800

    lintr suggested cleanup

--- R/read_file.R
+++ R/read_file.R
@@ -6,47 +6,58 @@
 #' are in the second column in the file.
 #'
 #' @param path The name of the fle which the data are to be read from.
-#' @param col.names A logical value indicating whether the file contains the names
+#' @param col_names A logical value indicating whether the file contains the names
 #'  of the variables as its first line.
 #' @param delim The file field separator character.
 #' @return A data frame with two columns, where the first column is 'id' (gene
 #'  identifiers) and the second column is 'set.label' (gene set labels).
 
 
-read_file <- function(path, col.names, delim){
-  if(missing(path)){
+read_file <- function(path, col_names, delim){
+  if (missing(path)) {
     stop("File path must be provided")
-  }else{
-    if(!file.exists(path)){
+  } else {
+    if (!file.exists(path)) {
       stop("The file doesn't exist, please provide path to an existing file")
-    }else{
-      message(stringr::str_wrap(crayon::yellow("ATTENTION: For the file to be
-processed correctly ensure that the gene identifiers are in the first column and the
-gene set labels are in the second column.")))
-      if(missing(col.names)){
-        stop("Please provide TRUE (present) or FALSE (absent) for the 'col.names' parameter.")
-      }else{
-        col.names <- col.names
+    } else {
+      message(
+        stringr::str_wrap(
+          crayon::yellow("ATTENTION: For the file to be processed correctly
+                        ensure that the gene identifiers are in the first column
+                        and the gene set labels are in the second column.")
+          )
+        )
+      if (missing(col_names)) {
+        stop("Please provide TRUE (present) or FALSE (absent) for the
+             'col.names' parameter.")
+      } else {
+        col_names <- col_names
       }
-      if(missing(delim)){
+      if (missing(delim)) {
         stop("Please provide a value for 'delim' parameter.")
-      }else{
+      } else {
         delim <- delim
       }
-      dat <- read.delim(file = path, header = col.names, sep = delim,
+      dat <- read.delim(file = path, header = col_names, sep = delim,
                         stringsAsFactors = FALSE,
                         colClasses = "character",
                         row.names = NULL)
-      message(stringr::str_wrap(crayon::green(paste0("SUCCESS: Read the file '",
-                                                     path, "' with ", nrow(dat),
-                                                     " and ", ncol(dat),
-                                                     " columns.\n"))))
-      if(ncol(dat) > 2){
-        message(stringr::str_wrap(crayon::yellow("ATTENTION: found more than two columns in the file,
-                                  retaining only the first two.")))
+      message(
+        stringr::str_wrap(
+          crayon::green(
+            paste0("SUCCESS: Read the file '", path, "' with ",
+                   nrow(dat), " and ", ncol(dat), " columns.\n")
+            )
+          )
+        )
+      if (ncol(dat) > 2) {
+        message(
+          stringr::str_wrap(
+            crayon::yellow("ATTENTION: found more than two columns in the file,
+                           retaining only the first two.")))
         dat <- dat[, 1:2]
       }
-      colnames(dat) <- c("id", "set.label")
+      colnames(dat) <- c("id", "set_label")
       return(dat)
     }
   }
