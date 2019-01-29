b5bd1aaad4ed025fb9e1256cd6965e25e7bfa736
Vitalina Komashko <vitalina@gmail.com>
2019-01-29 10:29:16 -0800

    ran lintr on clean_data, changed the name of the variables to avoid periods, added spaces, limited the length of the lines to 80 characters

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,62 +1,67 @@
 #' Basic data cleaning before gene mapping
-#' 
+#'
 #' `clean_data` function removes duplicated rows, sets that have fewer genes
-#' than `min.set.size`.
+#' than `min_set_size`.
 #'
 #' @param dat Data frame object with two columns: genes (`id`) and set labels (`set.name`),
 #'  output of the function \code{\link{read_data}} call.
-#' @param min.set.size Positive integer. If provided will be used to remove
+#' @param min_set_size Positive integer. If provided will be used to remove
 #'  all gene sets where then number of genes is fewer than \code{min.set.size}.
 #'
 #' @return Data frame object
 #' @export
 
 
-clean_data <- function(dat, min.set.size){
+clean_data <- function(dat, min_set_size){
   # check the data for duplicated rows
-  if(any(duplicated(dat))){
-    message(stringr::str_wrap(crayon::yellow("ATTENTION: Found duplicated rows in 
-                                             provided data, removing duplicates.")))
+  if (any(duplicated(dat))) {
+    message(stringr::str_wrap(crayon::yellow("ATTENTION: Found duplicated rows
+                                             in provided data, removing
+                                             duplicates.")))
     dat <- unique(dat)
     message(stringr::str_wrap(paste0("Number of rows after removing
-                                             duplicated rows is ", nrow(dat), ".")))
+                                     duplicated rows is ", nrow(dat), ".")))
   }
-  # remove modules 
-  if(!missing(min.set.size)){
-    if(min.set.size <= 0){
-      stop(stringr::str_wrap(paste0("You provided ", min.set.size, 
-                                    " as 'min.set.size'. Parameter 'min.set.size' 
-                                    should be a positive number.")))
-    }else{
-      message(stringr::str_wrap(paste0("Total number of sets in the dataset is ", 
-                                       length(unique(dat$set.)), ".")))
-      gene.count.by.set <- dat %>% group_by(.data$set.label) %>% 
-        summarise(n = n())
-      low.count.set.list <- gene.count.by.set %>% 
-        filter(.data$n <= min.set.size)
-      low.count.set.total <- low.count.set.list %>% summarize(total = n())
-      message(paste0("You provided ", min.set.size, 
-                                       " genes as 'min.set.size'. \nFound ", 
-                                       as.character(low.count.set.total), 
-                                       " sets with number of genes less than ", 
-                                       min.set.size, "."))
-      if(as.numeric(low.count.set.total) > 0){
-        dat <- dat[!(dat$set.label %in% low.count.set.list$set.label), ]
-        if(nrow(dat) == 0){
-          stop("After removing clusters with fewer than 'min.set.size' no rows remained in the data.")
+  # remove modules
+  if (!missing(min_set_size)) {
+    if (min_set_size <= 0){
+      stop(stringr::str_wrap(paste0("You provided ", min_set_size,
+                                    " as 'min_set_size'.
+                                    Parameter 'min_set_size' should be a
+                                    positive number.")))
+    } else {
+      message(stringr::str_wrap(paste0("Total number of sets in the dataset
+                                       is ", length(unique(dat$set.)), ".")))
+      gene_count_by_set <- dat %>%
+        dplyr::group_by(.data$set_label) %>%
+        dplyr::summarise(n = n())
+      low_count_set_list <- gene_count_by_set %>%
+        dplyr::filter(.data$n <= min_set_size)
+      low_count_set_total <- low_count_set_list %>%
+        dplyr::summarize(total = n())
+      message(paste0("You provided ", min_set_size,
+                                       " genes as 'min_set_size'. \nFound ",
+                                       as.character(low_count_set_total),
+                                       " sets with number of genes less than ",
+                                       min_set_size, "."))
+      if (as.numeric(low_count_set_total) > 0) {
+        dat <- dat[!(dat$set_label %in% low_count_set_list$set_label), ]
+        if (nrow(dat) == 0) {
+          stop("After removing clusters with fewer than 'min.set.size' no
+               rows remained in the data.")
         }
-        message(stringr::str_wrap(crayon::yellow(paste0("ATTENTION: Removed sets where 
-                                                      the number of genes is less than ", 
-                                                        min.set.size, 
-                                                        ". Number of rows after filtering is ", 
-                                                        nrow(dat), ". Number of sets after filtering is ",
-                                                        length(unique(dat$set.label)), "."))))
-      }else{
+        message(stringr::str_wrap(
+crayon::yellow(paste0("ATTENTION: Removed sets where the number of genes is
+                      less than ", min_set_size, ". Number of rows after
+                      filtering is ", nrow(dat), ". Number of sets after
+                      filtering is ", length(unique(dat$set_label)), ".")))
+)
+      } else {
         message("No sets will be removed.")
       }
     }
-  }else{
-    message(stringr::str_wrap("Parameter 'min.set.size' is not provided. No gene
+  } else {
+    message(stringr::str_wrap("Parameter 'min_set_size' is not provided. No gene
                               sets will be removed."))
   }
   return(dat)
