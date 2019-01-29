24185a78e6f24d74b1ab103db7f704f24c406774
Vitalina Komashko <vitalina@gmail.com>
2019-01-29 10:38:08 -0800

    lintr help: added spaces, renamed variables to avoid periods, limited the number of charactes in lines

--- R/get_ontology.R
+++ R/get_ontology.R
@@ -1,33 +1,35 @@
 #' Run analysis for a specified ontology
-#' 
-#' @param gene.id set of unique Entrez Gene identifiers for which GO annotations
+#'
+#' @param gene_id set of unique Entrez Gene identifiers for which GO annotations
 #' need to be calculated
 #' @param background set of unique Entrez Gene identifiers
 #' @param ontology a character string specifying the ontology, must be either "BP",
-#' "CC" or "MF" 
-#' @param module a string to add as a column to the final result. Might
+#' "CC" or "MF"
+#' @param gene_set_label a string to add as a column to the final result. Might
 #' be used as identification of the results in case there are many generated
 #' using this function
 #' @param annotation a string indicating annotation data package name
 #' shouldn't be exported or maybe it should?
 
 
-get_ontology <- function(gene.id, background, annotation, ontology, module){
-  params <- new("GOHyperGParams", geneIds = gene.id,
-                universeGeneIds = background, 
+get_ontology <- function(gene_id, background, annotation,
+                         ontology, gene_set_label){
+  params <- new("GOHyperGParams", geneIds = gene_id,
+                universeGeneIds = background,
                 annotation = annotation,
-                ontology = ontology, 
-                pvalueCutoff = 1, 
-                conditional = FALSE, 
+                ontology = ontology,
+                pvalueCutoff = 1,
+                conditional = FALSE,
                 testDirection = "over")
-  hgOver <- GOstats::hyperGTest(params)
-  hgOver.results <- GOstats::summary(hgOver)
-  hgOver.results <- hgOver.results %>% 
-    dplyr::mutate(P.Bonferroni = p.adjust(.data$Pvalue, method="bonferroni"),
-                            Ontology = ontology)
-  colnames(hgOver.results)[1] <- "GOID"
-  if(!missing(module)){
-    hgOver.results <- hgOver.results %>% dplyr::mutate(module = module)
+  hg_over_test <- GOstats::hyperGTest(params)
+  hg_over_results <- GOstats::summary(hg_over_test)
+  hg_over_results <- hg_over_results %>%
+    dplyr::mutate(p_bonferroni = p.adjust(.data$Pvalue, method = "bonferroni"),
+                            ontology = ontology)
+  colnames(hg_over_results)[1] <- "GOID"
+  if (!missing(gene_set_label)) {
+    hg_over_results <- hg_over_results %>%
+      dplyr::mutate(gene_set_label = gene_set_label)
   }
-  return(hgOver.results)
+  return(hg_over_results)
 }
