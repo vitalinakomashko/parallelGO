bb7fd0b9183e1600e4a58832b71c15870a93cfd6
Vitalina Komashko <vitalina@gmail.com>
2019-01-30 09:39:50 -0800

    these functions have been moved to the file R/get_data.R

--- R/bring_the_universe.R
+++
@@ -1,4 +0,0 @@
-# function to either read a list of genes that should be used as the "universe"
-# bucket for the hypergeometric test or to take the existing genes and clean
-# them for calculations
-#
