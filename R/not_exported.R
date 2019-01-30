f63caae73e953675c8ec2f01db1e69dbd6abf754
Vitalina Komashko <vitalina@gmail.com>
2019-01-30 09:41:41 -0800

    created a file to store misc functions that won't be exported to the user

---
+++ R/not_exported.R
@@ -0,0 +1,32 @@
+# ---------verify_input----------
+#' Supporting function for verifying function parameters
+#'
+#' Not to be used by the user. Not exported
+
+verify_input <- function(input_name, input_choices, input_default){
+  if (!missing(input_name)) {
+    if (length(input_name) == 1) {
+      if (input_name %in% input_choices) {
+        return(input_name)
+      } else {
+        stop(
+          paste0("Unexpected value for ", input_name,
+                 " was provided; please provide one of ",
+                 paste0(input_choices, collapse = " or "), ".")
+        )
+      }
+    } else {
+      stop(
+        paste0("More than one value for ", input_name,
+               " parameter was provided; please provide either ",
+               paste0(input_choices, collapse = " or "), "."))
+    }
+  } else {
+    message(
+      paste0("Using default setting for the parameter ",
+             input_name, ": ",
+             input_default)
+    )
+    return(input_default)
+  }
+}
