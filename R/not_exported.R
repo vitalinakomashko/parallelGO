638cead7ffaec58be25db729397c60846b062b21
Vitalina Komashko <vitalina@gmail.com>
2019-02-18 19:43:07 -0800

    updated documentation

--- R/not_exported.R
+++ R/not_exported.R
@@ -2,10 +2,10 @@
 #' Verify validity of the provided function input.
 #'
 #' `verify_input` provides checks given the name of the input parameter,
-#' possible choices and the default choice.  If the input_name is
-#' not missing and is valid it is then returned; if the input_name is not
-#' missing and not valid the execution is stopped; if the input_name is missing
-#' the input_default is then returned.
+#' possible choices and the default choice.  If the `input_name` is
+#' not missing and is valid it is then returned; if the `input_name` is not
+#' missing and not valid the execution is stopped; if the `input_name` is missing
+#' the `input_default` is then returned. This is an internal function.
 #'
 #' @param input_name String containing the parameter name.
 #' @param input_choices String containing the possible choices.
@@ -48,14 +48,15 @@ verify_input <- function(input_name, input_choices, input_default){
 #' Output a data frame for sets that generated an error or a warning
 #'
 #' `return_error_result` creates a one-row data frame with the same columns
-#' as the regular output of get_ontology. The `count`` is represented by the
-#' number of genes, the `term` provides the message and `set_label` provides
-#' the label of the gene set for which a condition was encountered.
+#' as the regular output of get_ontology. The column \strong{count} is
+#' represented by the number of genes, the column \strong{term} provides the
+#' message, and the column \strong{set_label} provides the label of the gene set
+#' for which a condition was encountered. This is an internal function.
 #'
 #' @seealso \code{\link{run_parallel_go}}, \code{\link{remove_errors}},
 #'  \code{\link{get_ontology}}.
 #'
-#'  @return Data frame with 1 row.
+#' @return Data frame with 1 row.
 
 return_error_result <- function(error_warning, input_genes, input_label){
   df <- data.frame(goid = NA,
@@ -76,15 +77,15 @@ return_error_result <- function(error_warning, input_genes, input_label){
 # ---------remove_errors----------
 #' Remove sets which generated errors or warnings by `get_all_ontologies`.
 #'
-#' `remove_errors` removes rows in the output of get_all_ontologies for sets
+#' `remove_errors` removes rows in the output of `get_all_ontologies` for sets
 #' which generated errors or warnings. Also provides a helpful message listing
 #' the labels of the removed sets. This is an internal function.
 #'
 #' @param dat Data frame, output of the parallelized run.
 #'
-#' @seealso \code{\link{run_parallel_go}}
+#' @seealso \code{\link{run_parallel_go}}.
 #'
-#' @return data frame.
+#' @return Data frame.
 
 remove_errors <- function(dat){
   if("error_warning" %in% dat$ontology) {
