2a35a87de8a46301f12f2e0b3afd12e254d26be3
Vitalina Komashko <vitalina@gmail.com>
2019-02-20 12:28:44 -0800

    updated documentation for the clean_data file and containing functions

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,17 +1,18 @@
-#' Basic data cleaning before gene mapping
+# --------- deduplicate_rows ----------
+#' Remove duplicated rows.
 #'
-#' `clean_data` function removes duplicated rows, sets that have fewer genes
-#' than `min_set_size`.
+#' \code{deduplicate_rows} function removes duplicated rows prior to mapping
+#' to ENTREZ gene identifiers.
 #'
-#' @param dat Data frame object with two columns: genes (`id`) and set
-#'  labels (`set_label`), output of the function \code{\link{read_data}} call.
+#' @param dat Data frame object with two columns: \strong{id} and set
+#'  labels \strong{set_label}.
 #'
-#' @return Data frame object
+#' @return Data frame.
+#'
 #' @export
 
 
-clean_data <- function(dat){
-  # check the data for duplicated rows
+deduplicate_rows <- function(dat){
   if (any(duplicated(dat))) {
     message(
       stringr::str_wrap(
@@ -22,10 +23,12 @@ clean_data <- function(dat){
     dat <- unique(dat)
     message(
       stringr::str_wrap(
-        paste0("Number of rows after removing duplicated rows is ",
-               nrow(dat), ".")
+        crayon::green(
+          paste0("Number of rows after removing duplicated rows is ",
+                 crayon::underline(nrow(dat)), ".")
         )
       )
+    )
   } else {
     message(
       stringr::str_wrap(
@@ -36,72 +39,92 @@ clean_data <- function(dat){
   return(dat)
 }
 
-
-#' Remove sets that have smaller number of members than provided threshold
+# --------- remove_small_sets ----------
+#' Remove gene sets where membership is smaller than a cutoff
 #'
-#' `remove_small_sets` function removes sets that have fewer genes
-#' than `min_set_size`.
+#' \code{remove_small_sets} function removes sets that have fewer genes
+#' identifiers than \code{min_set_size}.
 #'
-#' @param dat Data frame object with two columns: Entrez gene idenitifiers
-#'  (`id`) and set labels (`set_label`), output of the function
-#'  \code{\link{map_genes}} call.
-#' @param min_set_size Positive integer. If provided will be used to remove
-#'  all gene sets where then number of genes is fewer than \code{min_set_size}.
+#' @param dat Data frame object with two columns: ENTREZ gene idenitifiers
+#'  \code{entrez} and set labels \code{set_label}.
+#'
+#' @param min_set_size Positive integer that defines the minimum number of genes
+#' in a set. All sets that don't meet this criteria will be removed.
 #'
 #' @return Data frame object.
+#'
 #' @export
 
 remove_small_sets <- function(dat, min_set_size){
-  # remove modules
   if (!missing(min_set_size)) {
     if (min_set_size <= 0){
       stop(
         stringr::str_wrap(
-          paste0("You provided ", min_set_size,
-                 " as 'min_set_size'. Parameter 'min_set_size' should be a
-                 positive number.")))
+          crayon::red(
+            paste0("You provided ", crayon::underline(min_set_size),
+                   " as 'min_set_size'. Parameter 'min_set_size' ",
+                   "needs to be a positive number.")
+          )
+        )
+      )
     } else {
       message(
         stringr::str_wrap(
-          paste0("Total number of sets in the dataset is ",
-                 length(unique(dat$set_label)), ".")
+          crayon::green(
+            paste0("Total number of sets in the dataset is ",
+                   crayon::underline(length(unique(dat$set_label))), ".")
+          )
         )
       )
+      # calculate the number of genes in each set
       gene_count_by_set <- dat %>%
         dplyr::group_by(.data$set_label) %>%
         dplyr::summarise(n = n())
+      # extract sets where membership is less than min_set_size
       low_count_set_list <- gene_count_by_set %>%
         dplyr::filter(.data$n <= min_set_size)
+      # calculate how many sets need to be removed
       low_count_set_total <- low_count_set_list %>%
         dplyr::summarize(total = n())
       message(
-        paste0("You provided ", min_set_size,
-               " genes as 'min_set_size'. \nFound ",
-               as.character(low_count_set_total),
-               " sets with number of genes less than ", min_set_size, "."))
+        stringr::wrap(
+          crayon::green(
+            paste0("You provided ", crayon::underline(min_set_size),
+                   " genes as 'min_set_size'. Found ",
+                   crayon::underline(as.character(low_count_set_total)),
+                   " sets with number of genes less than ",
+                   crayon::underline(min_set_size), ".")
+          )
+        )
+      )
       if (as.numeric(low_count_set_total) > 0) {
         dat <- dat[!(dat$set_label %in% low_count_set_list$set_label), ]
         if (nrow(dat) == 0) {
-          stop("After removing clusters with fewer than 'min.set.size' no
-               rows remained in the data.")
+          stop(crayon::red("After removing clusters with fewer than
+                           'min.set.size' no rows remained in the data."))
         }
         message(
           stringr::str_wrap(
             crayon::yellow(
               paste0("ATTENTION: Removed sets where the number of genes is
-                     less than ", min_set_size, ". Number of rows after
-                     filtering is ", nrow(dat), ". Number of sets after
-                     filtering is ", length(unique(dat$set_label)), "."))
+                     less than ", crayon::underline(min_set_size),
+                     ". Number of rows after filtering is ",
+                     crayon::underline(nrow(dat)),
+                     ". Number of sets after filtering is ",
+                     crayon::underline(length(unique(dat$set_label))), "."))
               )
               )
         } else {
-          message("No sets will be removed.")
+          message(crayon::green("No sets will be removed."))
       }
     }
     } else {
       message(
-        stringr::str_wrap("Parameter 'min_set_size' is not provided.
-                          No gene sets will be removed."))
+        stringr::str_wrap(
+          crayon::green("Parameter 'min_set_size' is not provided.
+                        No gene sets will be removed.")
+          )
+        )
     }
   return(dat)
 }
