fdfd845a819d74e1fbb8904cc322d4183e047ada
Vitalina Komashko <vitalina@gmail.com>
2019-02-21 10:35:56 -0800

    finished writing the function for filtering p values and generated documentation

--- R/filter_pvalues.R
+++ R/filter_pvalues.R
@@ -1,8 +1,25 @@
 # --------- filter_pvalues ----------
 
-#' Filter results based on a p-value cutoff
+#' Filter GO enrichment results using a p-value cutoff
+#'
+#' \code{filter_pvalues} takes a data frame with the results of GO enrichment
+#' analysis and performs row filtering based on provided \code{cutoff} value.
+#' The data can be filtered either using the raw (\strong{pvalue} column) or the
+#' adjusted (default) p-values (\strong{p_bonferroni} column).
+#'
+#' @param dat Data frame with the results of GO enrichment analysis.
+#'
+#' @param which_pvalue A character string specifying p-value for filtering.
+#'  Must of one of two: "raw" or "adjusted"
+#'
+#' @param cutoff A numeric value specifying the cutoff. All rows with p-values
+#'  that are less or equal to the cutoff will be retained.
+#'
+#' @return Data frame.
+#'
+#' @seealso \code{get_ontology}.
 
-filter_pvalues <- function(dat, which_pvalue = c("raw", "adjusted"), cutoff){
+filter_pvalues <- function(dat, which_pvalue = "adjusted", cutoff){
   if (missing(cutoff)) {
     message(
       crayon::green("No filtering on p-values will be performed.")
@@ -17,9 +34,26 @@ filter_pvalues <- function(dat, which_pvalue = c("raw", "adjusted"), cutoff){
                  "less than 1.")
         )
       )
+    } else {
+      pval_type <- verify_input(input_name = which_pvalue,
+                                input_choices = c("raw", "adjusted"),
+                                input_default = "adjusted")
+      column_filter <- switch(pval_type, "raw" = "pvalue",
+                              "adjusted" = "p_bonferroni")
+      dat_filtered <- dat[dat[[column_filter]] <= cutoff, ]
+      if (nrow(dat_filtered) == 0){
+        message(
+          crayon::yellow(
+            paste0("ATTENTION: Using p-value cutoff on ",
+                   crayon::underline(column_filter), " column and cutoff ",
+                   crayon::underline(cutoff), " resulted in a data frame with ",
+                   "0 rows. Returning original data frame with unfiltered ",
+                   "p-values."))
+        )
+        return(dat)
+      } else {
+        return(dat_filtered)
+      }
     }
-    pval_type <- verify_input(input_name = which_pvalue,
-                              input_choices = c("raw", "adjusted"),
-                              input_default = "adjusted")
   }
 }
