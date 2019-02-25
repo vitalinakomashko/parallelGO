1f6bd68f256d3e475030aab02e227d3c979aea40
Vitalina Komashko <vitalina@gmail.com>
2019-02-25 14:57:46 -0800

    major update in the function: using annotate::lookUp instead of toTable function to retrieve ENTREZ ids

--- R/map_genes.R
+++ R/map_genes.R
@@ -1,12 +1,12 @@
 #' Map gene identifiers to ENTREZ gene
 #'
-#' \code{map_genes} provides mapping to ENTREZ gene identifiers for Hugo or Ensembl
-#' gene identifiers using either \href{https://www.bioconductor.org/packages/release/data/annotation/html/org.Mm.eg.db.html}{org.Mm.eg.db} or \href{https://www.bioconductor.org/packages/release/data/annotation/html/org.Hs.eg.db.html}{org.Hs.eg.db} Bioconductor annotation
-#' packages. Function \code{\link[AnnotationDb]{toTable}} is used to
+#' \code{map_genes} provides mapping to ENTREZ gene identifiers for Hugo or
+#' Ensembl gene identifiers using either \href{https://www.bioconductor.org/packages/release/data/annotation/html/org.Mm.eg.db.html}{org.Mm.eg.db} or \href{https://www.bioconductor.org/packages/release/data/annotation/html/org.Hs.eg.db.html}{org.Hs.eg.db} Bioconductor annotation
+#' packages. Function \code{\link[annotate]{lookUp}} is used to
 #' extract the mappings. We use org.Xx.eg.db::org.Xx.egALIAS2EG for the mappings
-#' between the common gene symbol idenditifers and org.Xx.eg.db::org.Xx.egENSEMBL for
-#' the mappings between ENTREZ Gene identifiers and Exnsembl gene
-#' accession numbers.
+#' between the common gene symbol idenditifers and
+#' org.Xx.eg.db::org.Xx.egENSEMBL for the mappings between ENTREZ gene
+#' identifiers and Ensembl gene accession numbers.
 #'
 #' @param dat Data frame with two columns: \strong{id} and \strong{set_label}.
 #'
@@ -41,90 +41,119 @@ map_genes <- function(dat, id = "symbol", species = "human"){
   id <- verify_input(input_name = id, input_choices = c("symbol", "ensembl"),
                      input_default = "symbol")
   if (species == "mouse") {
-    if (id == "symbol") {
-      xx <- AnnotationDbi::toTable(org.Mm.eg.db::org.Mm.egALIAS2EG)
-    } else {
-      xx <- AnnotationDbi::toTable(org.Mm.eg.db::org.Mm.egENSEMBL)
-    }
+    metadata_basename <- "org.Mm.eg"
+  } else {
+    metadata_basename <- "org.Hs.eg"
   }
-  if (species == "human") {
-    if (id == "symbol") {
-      xx <- AnnotationDbi::toTable(org.Hs.eg.db::org.Hs.egALIAS2EG)
-    } else {
-      xx <- AnnotationDbi::toTable(org.Hs.eg.db::org.Hs.egENSEMBL)
-    }
-  }
-  # check if ensembl id has versions in it and remove them
-  if (id == "ensembl") {
+  if (id == "symbol") {
+    annotation_element <- "ALIAS2EG"
+  } else {
+    annotation_element <- "ENSEMBL"
+    # also, remove versions in ENSEMBL ids in the input dat
     dat$id <- stringr::str_remove(dat$id, "\\..{1,}")
   }
-  colnames(xx) <- c("entrez", "mapping_id")
-  # since there could be multiple matches, do alphabetic sort
-  xx <- xx[order(xx$mapping_id), ]
-  # match ids in the data to the mapping_id in the annotation data
-  # frame extracting only the first match
-  k <- match(dat$id, xx$mapping_id)
-  dat_with_match <- dplyr::mutate(dat, index = k)
-  # remove rows without any match if such are available
-  if(any(is.na(k))){
-    dat_with_match <- dat_with_match[!is.na(dat_with_match$index), ]
-  }
-  # add the matched entrez ID and select relevant columns
-  dat_with_genes <- dplyr::bind_cols(dat_with_match,
-                                     xx[dat_with_match$index, ]) %>%
-    dplyr::select(.data$entrez, .data$set_label)
-  if (nrow(dat_with_genes) == 0) {
-    stop(
-      stringr::str_wrap(
-        crayon::red(
-          paste0("ERROR: The number of rows after mapping identifiers to ENTREZ",
-                 " gene identifiers is 0. Please verify that you used correct",
-                 " input values for 'species' and 'id' parameters. You used ",
-                 crayon::underline(species),
-                 " for the 'species' parameter and ",
-                 crayon::underline(id), " for the 'id' parameter.")
+  # this returns a list, where the name is the symbol/ensembl and the value is
+  # the character vector of all ENTREZ ids this symbol maps to.
+  xx <- annotate::lookUp(dat$id, metadata_basename, annotation_element,
+                         load = TRUE)
+  # each symbol/ensembl might map to more than one ENTREZ id.
+  map_length <- sapply(xx, length)
+  map_count <- data.frame(matches = unname(map_length), id = names(map_length))
+  # summarize
+  map_summary <- map_count %>%
+    dplyr::group_by(.data$matches) %>%
+    dplyr::summarise(count = n())
+  message(
+    stringr::str_wrap(
+      crayon::green(
+        paste0("Summary of the mapping results from the provided ",
+               crayon::underline(id), " to ENTREZ gene identifiers:")
+      )
+    )
+  )
+  print(map_summary)
+  message(
+    stringr::str_wrap(
+      crayon::green(
+        paste0("If more than 1 matches are found to ENTREZ ids, only the ",
+               "first match is used.")
+      )
+    )
+  )
+  # extract the first matches only
+  first_match_entrez <- sapply(xx, "[", 1)
+  # add ENTREZ ids to dat
+  dat_with_match <- dplyr::mutate(dat, entrez = unname(first_match_entrez))
+  # drop "id" column
+  dat_with_match <- dat_with_match[, c("entrez", "set_label")]
+  # remove rows with missing ENTREZ (where ENTREZ wasn't found)
+  if(any(is.na(dat_with_match$entrez))) {
+    k <- which(is.na(dat_with_match$entrez))
+    dat_with_match <- dat_with_match[!is.na(dat_with_match$entrez), ]
+    if (nrow(dat_with_match) == 0) {
+      stop(
+        stringr::str_wrap(
+          crayon::red(
+            paste0("ERROR: The number of rows after mapping identifiers to ",
+                   "ENTREZ gene identifiers is 0. Please verify that you used ",
+                   "correct input values for 'species' and 'id' parameters. ",
+                   "You used ", crayon::underline(species),
+                   " for the 'species' parameter and ",
+                   crayon::underline(id), " for the 'id' parameter.")
           )
         )
       )
-  } else if (nrow(dat_with_genes) < nrow(dat) / 2) {
-    warning(
-      stringr::str_wrap(
-        crayon::yellow(
-          paste0("ATTENTION: The number of rows after mapping identifiers
-                  to ENTREZ gene identifiers is significantly smaller than
-                  in the original data provided. Please verify that you used
-                  correct input values for 'species' or 'id' parameters.
-                  You used ", crayon::underline(species), " for the 'species'
-                  parameter and ", crayon::underline(id),
-                 " for the 'id' parameter.")
+    } else if (nrow(dat_with_match) < nrow(dat) / 2) {
+      warning(
+        stringr::str_wrap(
+          crayon::yellow(
+            paste0("ATTENTION: The number of rows after mapping identifiers ",
+                   "to ENTREZ gene identifiers is less than a half of the ",
+                   " original identifiers provided.  Please verify that you ",
+                   "used correct input values for 'species' or 'id' ",
+                   "parameters. You used ", crayon::underline(species),
+                   " for the 'species' parameter and ", crayon::underline(id),
+                   " for the 'id' parameter.")
           )
         )
       )
-    return(dat_with_genes)
+    } else {
+      message(
+        stringr::str_wrap(
+          crayon::green(
+            paste0("SUCCESS: Performed mapping to ENTREZ gene identifiers. ",
+                   crayon::underline(nrow(dat_with_match)),
+                   " out of the original ",
+                   crayon::underline(nrow(dat)), " identifiers were mapped.")
+          )
+        )
+      )
+    }
   } else {
     message(
       stringr::str_wrap(
         crayon::green(
           paste0("SUCCESS: Performed mapping to ENTREZ gene identifiers. ",
-                 crayon::underline(nrow(dat_with_genes)),
-                 " out of the original ",
-                 crayon::underline(nrow(dat)), " identifiers were mapped.")
+                 "All identifiers have been mapped")
         )
       )
     )
-    if (any(duplicated(dat_with_genes))) {
-      dat_with_genes <- unique(dat_with_genes)
-      message(
-        stringr::str_wrap(
-          crayon::yellow(
-            paste0("ATTENTION: Removed duplicated rows after mapping to ENTREZ",
-                   " gene identifiers. ",
-                   crayon::underline(nrow(dat_with_genes)),
-                   " rows remained.")
-          )
+  }
+  # remove duplicates
+  if (any(duplicated(dat_with_match))) {
+    dat_with_match <- unique(dat_with_match)
+    message(
+      stringr::str_wrap(
+        crayon::yellow(
+          paste0("ATTENTION: Removed duplicated rows after mapping to ENTREZ",
+                 " gene identifiers. ",
+                 crayon::underline(nrow(dat_with_match)),
+                 " rows remained.")
         )
       )
-    }
-    return(dat_with_genes)
+    )
   }
+  return(dat_with_match)
 }
+
+
